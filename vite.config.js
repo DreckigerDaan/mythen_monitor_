cd68b4e53a5b0f3d0d56248ce60f55faf9feb3ef
DreckigerDaan <65292718+DreckigerDaan@users.noreply.github.com>
2024-09-16 21:44:23 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => {
   return {
-    base: mode === 'production' ? '/mythen_monitor_/' : '/',
+    base: mode === 'production' ? '/mythen_monitor_/' : './',
     plugins: [react()]
   }
 })
